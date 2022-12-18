8b466e45b38ce7466f775ce4bb840abb5dfeb18f
kayskayskays <115312476+kayskayskays@users.noreply.github.com>
2022-12-19 04:47:03 +1100

    Listening to [.mn summary], added comments

--- hub.js
+++ hub.js
@@ -1,47 +1,52 @@
 
 "use strict";
 
-var grid = document.getElementById("grid-container");
-var gridStyle = getComputedStyle(grid);
-
-var gridRows = gridStyle.getPropertyValue("grid-template-rows").split(" ");
-var mainElements = document.getElementsByClassName("mn").length;
-
 var style, rowStart, bool, height, image, current, currentStyle, updated;
 
+var grid = document.getElementById("grid-container");
 
-// maybe change to setPropertyValue
+// adding an event listener to each main element
 document.querySelectorAll('.mn').forEach(item => {
-
-    item.addEventListener('click', event => {
+    
+    // listening for clicks on the summary
+    var summary = item.getElementsByTagName("summary")[0];
+    summary.addEventListener('click', event => {
         
+        // stores information about the current grid style
         currentStyle = getComputedStyle(grid);
         current = currentStyle.getPropertyValue("grid-template-rows").split(" ");
 
+        // stores information about the current main item style
         style = getComputedStyle(item);
         rowStart = style.getPropertyValue("grid-row-start");
-    
+        
+        // checks whether the item has been clicked 
         bool = item.getAttribute("clicked");
+
+        // finds images
         image = item.getElementsByTagName("img")[0];
-    
-        if (image) {
+        
+        if (image) { 
+        // changes to row height are only necessary if and image is present
             height = image['height'] + 100 + 'px';
 
+            // updating the new style
             updated = [...current];
-    
             updated[rowStart - 1] = height;
             updated = updated.join(" ");
+
+            // reverting current style
             current[rowStart - 1] = item.getAttribute('default');
             current = current.join(" ");
-    
-            if (bool == "true") {
+            
+            // setting the style
+            if (bool == "false") {
                 grid.setAttribute("style", "grid-template-rows: " + updated);
-                item.setAttribute("clicked", "false");
+                item.setAttribute("clicked", "true");
             } else {
                 grid.setAttribute("style", "grid-template-rows: " + current);
-                item.setAttribute("clicked", "true");
+                item.setAttribute("clicked", "false");
             }
         }
-
     })
   })
